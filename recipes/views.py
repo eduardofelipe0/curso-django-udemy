622dbfd776254288a54a2e2e6f1c70767ec6515f
Eduardo Felipe da Silva <eduardo.felipesilva99@gmail.com>
2026-05-14 13:00:01 -0300

    criacao da homepage e css de estilizacao, alem da criacao de block para reutilizar nas paginas

---
+++ recipes/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'recipes/home.html')
+
+def recipe(request, id):
+    return render(request, 'recipes/recipe-detail.html')
